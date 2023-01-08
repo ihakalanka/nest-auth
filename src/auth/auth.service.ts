f961595a66726abcdb78fabcb709e6cdf5538a59
ihakalanka <akalankaih19@gmail.com>
2023-01-09 01:12:31 +0530

    added create user function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,4 +4,16 @@ import { Model } from 'mongoose';
 import { User } from '../schemas/user.model';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel('User') private userModel: Model<User>) {}
+
+  async createUser(user: User): Promise<User> {
+    const createdUser = new this.userModel(user);
+    const userExists = await this.userModel.findOne({ email: user.email });
+    if (userExists) {
+      throw new BadRequestException('User already exists');
+    } else {
+      return await createdUser.save();
+    }
+  }
+}
