003a137993a313947cc669694353c3e433076b39
ihakalanka <akalankaih19@gmail.com>
2023-01-09 01:25:08 +0530

    hashed the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from '../schemas/user.model';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,6 +10,9 @@ export class AuthService {
 
   async createUser(user: User): Promise<User> {
     const createdUser = new this.userModel(user);
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(createdUser.password, salt);
+    createdUser.password = hash;
     const userExists = await this.userModel.findOne({ email: user.email });
     if (userExists) {
       throw new BadRequestException('User already exists');
