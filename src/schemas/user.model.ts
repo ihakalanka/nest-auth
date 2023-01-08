7f8a97a45cfff440fe369cb4ce35369506e37f32
ihakalanka <akalankaih19@gmail.com>
2023-01-08 15:18:10 +0530

    added user model

---
+++ src/schemas/user.model.ts
@@ -0,0 +1,27 @@
+import * as mongoose from 'mongoose';
+
+export const UserSchema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: {
+      validator: (v: string) => {
+        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
+  },
+  password: { type: String, required: true },
+  role: {
+    type: String,
+    required: true,
+    default: 'user',
+    enum: ['user', 'admin'],
+  },
+  createdAt: { type: Date, default: Date.now },
+});
