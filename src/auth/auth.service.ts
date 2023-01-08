74533c04563f881e196120054d08fd759460d7cf
ihakalanka <akalankaih19@gmail.com>
2023-01-09 01:25:23 +0530

    validate the password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -10,6 +10,7 @@ export class AuthService {
 
   async createUser(user: User): Promise<User> {
     const createdUser = new this.userModel(user);
+    this.validatePassword(createdUser, createdUser.password);
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(createdUser.password, salt);
     createdUser.password = hash;
@@ -18,6 +19,38 @@ export class AuthService {
       throw new BadRequestException('User already exists');
     } else {
       return await createdUser.save();
+    }
+  }
+
+  validatePassword(user: User, password: string) {
+    if (password.length < 6) {
+      throw new BadRequestException(
+        'Password must be at least 6 characters long',
+      );
+    } else if (password.length > 15) {
+      throw new BadRequestException(
+        'Password must be less than 15 characters long',
+      );
+    } else if (!password.match(/[a-z]/)) {
+      throw new BadRequestException(
+        'Password must contain at least one lowercase letter',
+      );
+    } else if (!password.match(/[A-Z]/)) {
+      throw new BadRequestException(
+        'Password must contain at least one uppercase letter',
+      );
+    } else if (!password.match(/[0-9]/)) {
+      throw new BadRequestException(
+        'Password must contain at least one number',
+      );
+    } else if (!password.match(/[!@#$%^&*]/)) {
+      throw new BadRequestException(
+        'Password must contain at least one special character',
+      );
+    } else if (password.match(/\s/)) {
+      throw new BadRequestException('Password must not contain any spaces');
+    } else {
+      return true;
     }
   }
 }
